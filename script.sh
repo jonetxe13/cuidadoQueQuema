23e40e902dfda1ea4b01e017520b2b8cd04762c9
jonetxe <jechevarria031@ikasle.ehu.es>
2025-05-14 18:07:56 +0200

    arreglado pid, npr en argumentos, packaging rarete y hecho version asincrona

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
-mpicc -o heat_p heat_p.c diffusion_p.c faux_p.c
-mpirun -mca btl openib,vader,self,tcp -mca mpi_cuda_support 0 -hostfile /export/home/nodos/hostfinal -map-by node -bind-to none -n $1 heat_p card0
+mpicc -o heat_pAsinc heat_pAsinc.c diffusion_pAsinc.c faux_pAsinc.c
+mpirun -mca btl openib,vader,self,tcp -mca mpi_cuda_support 0 -hostfile /export/home/nodos/hostfinal -map-by node -bind-to none -n $1 heat_pAsinc card0
 # ./heat_p card0
 # vfinder card0_par.res
 
