8f2315923d785f650c71745db784181f28a826d5
dalferr <da7172.da@gmail.com>
2025-05-15 17:56:06 +0200

    Comunicación inmediata optimizada

--- Serie/script.sh
+++ Serie/script.sh
@@ -2,7 +2,7 @@
 
 icc -o heat_s heat_s.c diffusion_s.c faux_s.c
 
-touch ../Resultados/resultados-serie.txt
+echo "" >../Resultados/resultados-serie.txt
 
 echo -e "Resultados serie\n" >>../Resultados/resultados-serie.txt
-./heat_s ../Cards/card
+./heat_s ../Cards/card >>../Resultados/resultados-serie.txt
